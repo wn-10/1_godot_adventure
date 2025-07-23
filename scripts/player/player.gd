f29a1985ee6355cbbebb074baa67e53bcec8a4e6
wn-10 <wn_10@qq.com>
2025-07-23 21:53:42 +0800

    设置玩家角色

---
+++ scripts/player/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var move_vector : Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	velocity = move_vector * move_speed
+	
+	# 判断玩家方向
+	# 当玩家向右移动时,x方向的的速度必须为正
+	# 当玩家向下移动时,y方向的的速度必须为正
+	if velocity.x > 0:
+		animated_sprite_2d.play("move_right")
+	elif velocity.x < 0:
+		animated_sprite_2d.play("move_left")
+	elif velocity.y > 0:
+		animated_sprite_2d.play("move_down")
+	elif velocity.y < 0:
+		animated_sprite_2d.play("move_up")
+	elif velocity == Vector2.ZERO:
+		animated_sprite_2d.stop()
+		
+	move_and_slide()
